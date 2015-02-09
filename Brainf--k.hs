ceaccccd39dba7a53de34c71594f4b302874f320
Nick Spinale <spinalen@carleton.edu>
2015-02-09 08:05:02 -0600

    done

--- Brainf--k.hs
+++ Brainf--k.hs
@@ -5,33 +5,25 @@ import qualified Data.ByteString as B
 import           Data.Word8
 import           System.Environment
 
-type Array = ([Word8], Word8, [Word8])
-
 main = do
     s <- fmap head getArgs >>= B.readFile
     case parseOnly eat s of
         Left str -> putStrLn str
-        Right f -> void $ f (repeat 0, 0, repeat 0)
+        Right f -> void $ f (repeat 0, 0 :: Word8, repeat 0)
 
-eat :: Parser (Array -> IO Array)
 eat = fmap (foldl (>=>) return) . many' $ choice
     [ char '<' ~> \(l:ls, m, rs) -> return (ls, l, m:rs)
     , char '>' ~> \(ls, m, r:rs) -> return (m:ls, r, rs)
-    , char '+' ~> toMid succ
-    , char '-' ~> toMid pred
-    , char '.' ~> \s -> print (mid s) >> return s
+    , char '+' ~> \(l, m, r) -> return (l, succ m, r)
+    , char '-' ~> \(l, m, r) -> return (l, pred m, r)
+    , char '.' ~> \s@(_, m, _) -> print m >> return s
     , char ',' ~> \(l, _, r) -> getLine >>= \i -> return (l, read i, r)
     , fmap block $ char '[' *> eat <* char ']'
-    , anyChar ~> return
+    , notChar ']' ~> return
     ]
 
-mid (_, y, _) = y
-toMid f (x, y, z) = return (x, f y, z)
+block _ s@(_, 0, _) = return s
+block f s = f s >>= block f
 
 infix 9 ~>
 v ~> c = c <$ v
-
-block :: (Array -> IO Array) -> Array -> IO Array
-block f s = case mid s of
-    0 -> return s
-    m -> f s >>= block f
