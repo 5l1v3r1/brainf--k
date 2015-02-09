f2ab9201231f05da7d25a5cd02c8800e069413b2
Nick Spinale <spinalen@carleton.edu>
2015-02-09 09:03:18 -0600

    done

--- Brainf--k.hs
+++ Brainf--k.hs
@@ -6,24 +6,24 @@ import           Data.Word8
 import           System.Environment
 
 main = do
-    s <- fmap head getArgs >>= B.readFile
+    s <- head <$> getArgs >>= B.readFile
     case parseOnly eat s of
         Left str -> putStrLn str
-        Right f -> void $ f (repeat 0, 0 :: Word8, repeat 0)
+        Right  f -> void $ f (repeat 0, 0 :: Word8, repeat 0)
 
 eat = fmap (foldl (>=>) return) . many' $ choice
-    [ char '<' ~> \(l:ls, m, rs) -> return (ls, l, m:rs)
-    , char '>' ~> \(ls, m, r:rs) -> return (m:ls, r, rs)
-    , char '+' ~> \(l, m, r) -> return (l, succ m, r)
-    , char '-' ~> \(l, m, r) -> return (l, pred m, r)
-    , char '.' ~> \s@(_, m, _) -> print m >> return s
-    , char ',' ~> \(l, _, r) -> getLine >>= \i -> return (l, read i, r)
+    [ char '<' ~> \  (l:ls, m,   rs) -> return (  ls, l     , m:rs)
+    , char '>' ~> \  (  ls, m, r:rs) -> return (m:ls, r     ,   rs)
+    , char '+' ~> \  (  ls, m,   rs) -> return (  ls, succ m,   rs)
+    , char '-' ~> \  (  ls, m,   rs) -> return (  ls, pred m,   rs)
+    , char '.' ~> \s@(   _, m,    _) -> print m >> return s
+    , char ',' ~> \  (  ls, _,   rs) -> getLine >>= \i -> return (ls, read i, rs)
     , fmap block $ char '[' *> eat <* char ']'
     , notChar ']' ~> return
     ]
 
 block _ s@(_, 0, _) = return s
-block f s = f s >>= block f
+block f s           = f s >>= block f
 
 infix 9 ~>
 v ~> c = c <$ v
