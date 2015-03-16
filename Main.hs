3492e15483f18ea93c0009ee4eeb437988bbb32d
Nick Spinale <spinalen@carleton.edu>
2015-03-16 09:26:23 -0500

    messed up

--- Main.hs
+++ Main.hs
@@ -1,11 +1,31 @@
-import           Data.Attoparsec.ByteString.Char8
-import qualified Data.Bytestring as B
+import Control.Monad.Trans.State.Lazy
+
+-- data Instr = Left | Right | Up | Down | In | Out | Block [Instr]
+
+-- follow :: [Instr] -> ([Word8], Word8, [Word8]) -> IO ()
+-- follow (Left:xs) (l:ls, m, rs) = follow xs (ls, l, m:rs)
+-- follow (Right:xs) (ls, m, r:rs) = follow xs (l:ls, r, m:rs)
+-- follow [] _ = return ()
 
 main = do
-    code <- getArgs >>= (B.readFile . head)
-    case parseOnly $ B.filter (`elem` "<>+-,.") code of
-        Left e -> putStrLn e
-        Right f -> void . execStateT . f 0 $ (repeat 0, repeat 0)
+    str <- getContents >>= (void . runStateT f . (++ "]"))
+    void $ runState f (str ++ "]") 0 `runStateT` (repeat 0, repeat 0)
+
+f :: State String (Word8 -> StateT ([Word8], [Word8]) IO Word8)
+f = do
+    p <- pop
+    case p of
+        -- '<' -> <=< \m -> StateT $ \(l:ls, rs) -> return (l, (ls, m:rs))
+        -- '>' -> \m -> StateT $ \(ls, r:rs) -> return (r, (m:ls, rs))
+        '+' -> (return . (+ 1) >=>) <$> f
+        -- '-' -> return . (- 1)
+        -- '.' -> \m -> liftIO putChar m >> return m
+        ',' -> (const (liftIO getChar) >=>) <$> f
+        '[' -> do n <- f
+                  m <- f
+                  return (block n >=> m)
+        ']' -> return
+        
 
 eat = foldr (>=>) return <$> choice
     [ char '<' *> \m -> StateT $ \(l:ls, rs) -> return (l, (ls, m:rs))
@@ -16,6 +36,8 @@ eat = foldr (>=>) return <$> choice
     , char '.' *> \m -> liftIO putChar m >> return m
     , fmap block $ char '[' *> eat <* char ']'
     ]
+
+pop = State \x:xs -> (x, xs)
 
 block s 0 = return 0
 block s m = s m >>= block s
